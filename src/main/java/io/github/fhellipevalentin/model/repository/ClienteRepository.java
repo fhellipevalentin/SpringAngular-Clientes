4cff7359b6bee1b817c3521e67fef7cec4c8c7c9
fhellipevalentin <fhellipereis@outlook.com>
2022-04-01 17:40:35 -0300

    Criando os Repositorios JPA

---
+++ src/main/java/io/github/fhellipevalentin/model/repository/ClienteRepository.java
@@ -0,0 +1,5 @@
+package io.github.fhellipevalentin.model.repository;
+
+public interface ClienteRepository {
+
+}
---
+++ src/main/java/io/github/fhellipevalentin/model/repository/ServicoRepository.java
@@ -0,0 +1,5 @@
+package io.github.fhellipevalentin.model.repository;
+
+public class ServicoRepository {
+
+}
