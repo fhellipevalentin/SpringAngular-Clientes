5de7829e8a59c1ece78a5fe86d3b94146880ecfd
fhellipevalentin <fhellipereis@outlook.com>
2022-04-01 16:44:43 -0300

    projeto criado e entidade cliente mapeada

---
+++ src/main/java/io/github/fhellipevalentin/model/entities/Cliente.java
@@ -0,0 +1,26 @@
+package io.github.fhellipevalentin.model.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 11)
+	private String nome;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	@Column
+	private LocalDate dataCadastro;
+}
