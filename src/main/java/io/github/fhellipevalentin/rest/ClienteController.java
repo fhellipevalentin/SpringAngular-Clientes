d1bf1186b751d433c99c2278bfb584fdcc1eea15
fhellipevalentin <fhellipereis@outlook.com>
2022-04-02 12:05:09 -0300

    GET para Cliente

--- src/main/java/io/github/fhellipevalentin/rest/ClienteController.java
+++ src/main/java/io/github/fhellipevalentin/rest/ClienteController.java
@@ -2,11 +2,14 @@ package io.github.fhellipevalentin.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.fhellipevalentin.model.entities.Cliente;
 import io.github.fhellipevalentin.model.repository.ClienteRepository;
@@ -26,5 +29,12 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente salvar( @RequestBody Cliente cliente) {
 		return repository.save(cliente);
+	}
+	
+	@GetMapping("{id}")
+	public Cliente acharPorId( @PathVariable Integer id) {
+		return repository
+				.findById(id)
+				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 }
