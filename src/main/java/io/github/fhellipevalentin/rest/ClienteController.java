6325cd58bcba22feb2049dbaef7fdf9487f100d8
fhellipevalentin <fhellipereis@outlook.com>
2022-04-02 11:37:27 -0300

    Iniciando a API de Clientes

---
+++ src/main/java/io/github/fhellipevalentin/rest/ClienteController.java
@@ -0,0 +1,29 @@
+package io.github.fhellipevalentin.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.fhellipevalentin.model.entities.Cliente;
+import io.github.fhellipevalentin.model.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+	
+	private final ClienteRepository repository;
+	
+	@Autowired
+	public ClienteController(ClienteRepository repository) {
+		this.repository = repository;
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(Cliente cliente) {
+		return repository.save(cliente);
+	}
+}
