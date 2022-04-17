df2697164058a4a266e93c5391f1cdcf4cf2cfaa
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-17 13:23:35 -0300

    Metodo de pesquisa de Servicos Prestados

--- src/main/java/io/github/fhellipevalentin/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/fhellipevalentin/rest/ServicoPrestadoController.java
@@ -2,11 +2,14 @@ package io.github.fhellipevalentin.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -48,4 +51,11 @@ public class ServicoPrestadoController {
 		servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
 		return servicoPrestadoRepository.save(servicoPrestado);
 	}
-}
+	@GetMapping
+	public List<ServicoPrestado> pesquisar(
+			@RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+			@RequestParam(value = "mes", required = false) Integer mes
+			){
+		return servicoPrestadoRepository.findByNomeClienteAndMes("%" + nome + "%", mes);
+		}
+	}
