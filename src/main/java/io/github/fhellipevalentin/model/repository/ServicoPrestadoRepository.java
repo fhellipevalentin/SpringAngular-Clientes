6340ec354ea2c70e951dad5a5b01c0399a243cb4
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-17 12:53:32 -0300

    Controlador e DTO para Servico Prestado

---
+++ src/main/java/io/github/fhellipevalentin/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,9 @@
+package io.github.fhellipevalentin.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.fhellipevalentin.model.entities.ServicoPrestado;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer>{
+
+}
--- src/main/java/io/github/fhellipevalentin/model/repository/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.fhellipevalentin.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.github.fhellipevalentin.model.entities.Servico;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer>{
-
-}
