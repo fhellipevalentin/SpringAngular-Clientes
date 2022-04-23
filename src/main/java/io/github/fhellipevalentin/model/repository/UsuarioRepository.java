40d85362ca4be1778bbe9a6785e95879011b45a1
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-22 23:49:08 -0300

    Autenticando com os usuários na base de dados

--- src/main/java/io/github/fhellipevalentin/model/repository/UsuarioRepository.java
+++ src/main/java/io/github/fhellipevalentin/model/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package io.github.fhellipevalentin.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.fhellipevalentin.model.entities.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-	
+	Optional<Usuario> findByUsername(String username);
 }
